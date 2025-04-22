5373796825a857e1f8e9faba8276d0e9927395d2
Vitaly <itassan6@gmail.com>
2025-04-22 12:54:53 +0300

    added cv

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -12,7 +12,6 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // simple server-side validation
   if (!validateString(senderEmail, 500)) {
     return {
       error: "Invalid sender email",
@@ -28,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      to: "babenko.vitaly12@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: createElement(ContactFormEmail, {
--- sections/HeroSection/HeroSection.tsx
+++ sections/HeroSection/HeroSection.tsx
@@ -53,7 +53,7 @@ function HeroSection({ content }: HeroSectionProps) {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/BabenkoCV.pdf"
           download
         >
           {content.cvButtonText}
